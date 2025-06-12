d73da8a36696cb45e8d525254e21f5a74d47f8c1
Jaeden Bardati <59208183+JaedenBardati@users.noreply.github.com>
2025-06-11 21:46:00 -0700

    added multiple wavelength and early stoppage support

--- espresso.cpp
+++ espresso.cpp
@@ -19,6 +19,7 @@
 #include <chrono>
 #include <concepts>
 #include <type_traits>
+#include <string>
 using namespace std;
 
 #include <Kokkos_Core.hpp>
@@ -59,15 +60,37 @@ int dump_vector_textfile(string filename, vector<T> x, vector<T> y, vector<T> z,
 }
 
 
+// INPUT SPECTRUM
+template <typename Tf>
+KOKKOS_INLINE_FUNCTION
+Tf invCDF_input_spectrum(Tf rand_uniform) {
+    return rand_uniform < 0.3333 ? 0.5 : (rand_uniform < 0.6666 ? 1 : 2);   // for now just flat spectrum with three wavelengths: 0.5, 1, 2
+}
+
 // DENSITY
 template <typename Tf>
 KOKKOS_INLINE_FUNCTION
 Tf density(Tf x, Tf y, Tf z) {
     Tf eps = 0.001;
-    Tf mag = 10.0;
+    Tf mag = 2.0;
     Tf norm = Kokkos::sqrt(x * x + y * y + z * z);
     return eps + Kokkos::max(mag * (1.0 - norm), 0.0);
 }
+
+//// OPACITY
+//template <typename Tf>
+//KOKKOS_INLINE_FUNCTION
+//Tf kappa_a(Tf wav) {
+//    Tf mag = 0.5;
+//    return mag;
+//}
+//
+//template <typename Tf>
+//KOKKOS_INLINE_FUNCTION
+//Tf kappa_s(Tf wav) {
+//    Tf mag = 0.5;
+//    return mag/pow(wav, 4);
+//}
 
 
 // PHOTON PACKETS
@@ -78,9 +101,10 @@ struct PhotonPackets {
     Kokkos::View<Tf*> locations;
     Kokkos::View<Tf*> directions;
     Kokkos::View<Tf*> weights;
+    Kokkos::View<Tf*> wavelengths;
     Kokkos::View<bool*> isalive;
 
-    PhotonPackets(unsigned int dim, Tu allocated_space): dim(dim), locations("locations", dim * allocated_space), directions("directions", dim * allocated_space), weights("weights", allocated_space), isalive("isalive", allocated_space) {
+    PhotonPackets(unsigned int dim, Tu allocated_space): dim(dim), locations("locations", dim * allocated_space), directions("directions", dim * allocated_space), weights("weights", allocated_space), wavelengths("wavelengths", allocated_space), isalive("isalive", allocated_space) {
         num = Kokkos::View<Tu>("num");
         Kokkos::deep_copy(num, 0);
     }
@@ -89,6 +113,7 @@ struct PhotonPackets {
         Kokkos::View<Tf*, Kokkos::HostSpace> locations;
         Kokkos::View<Tf*, Kokkos::HostSpace> directions;
         Kokkos::View<Tf*, Kokkos::HostSpace> weights;
+        Kokkos::View<Tf*, Kokkos::HostSpace> wavelengths;
         Kokkos::View<bool*, Kokkos::HostSpace> isalive;
         Kokkos::View<Tu, Kokkos::HostSpace> num;
     };
@@ -98,6 +123,7 @@ struct PhotonPackets {
         host.locations = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), locations);
         host.directions = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), directions);
         host.weights = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), weights);
+        host.wavelengths = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), wavelengths);
         host.isalive = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), isalive);
         host.num = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), num);
         return host;
@@ -133,14 +159,16 @@ int main(int argc, char** argv) {
     FLOAT kappa = kappa_a + kappa_s;
     FLOAT dt = frac_of_mfpovc*1.0/(kappa*(10.01)*speed_of_light); // timestep
     
-    UINT NPACKETS = 1e6*dt/0.05;
+    UINT NPACKETS = 1e7*dt/0.05;
     UINT allocated_space = NPACKETS*max_time/dt; // very conservative allocation: will definitely not be more than this (assumes no interactions)
+    
+    FLOAT cnv_tol = 1e-5;
     
     const FLOAT XINIT = 2.0;
     const FLOAT YINIT = 0.0;
     const FLOAT ZINIT = 0.0;
     
-    const bool photon_dump = false;
+    const bool photon_dump = true;
     const bool zslice_dump = true;
     
     /* ****** SIMULATION ****** */
@@ -156,7 +184,6 @@ int main(int argc, char** argv) {
         
         // reserve space for photon packets
         PhotonPackets<FLOAT, UINT> packets(DIM, allocated_space);
-    
         
         cout << "Running simulation ..." << endl;
         auto start = chrono::high_resolution_clock::now();
@@ -171,6 +198,8 @@ int main(int argc, char** argv) {
             cout << "time = " << fixed << setprecision(6) << time << "; nphotons=" << host_packets_num << endl;
             #endif
             
+            UINT old_packets_num = packets.num();
+            
             // spawn source packets randomly
             #if DEBUG >= 2
             cout << " spawning..." << endl;
@@ -180,6 +209,7 @@ int main(int argc, char** argv) {
                 auto rand_gen = rand_pool.get_state();
                 FLOAT phi_i = rand_gen.frand(0.0, TWO_PI);
                 FLOAT costheta_i = rand_gen.frand(-1.0, 1.0);
+                FLOAT r_uniform = rand_gen.frand(0.0, 1.0);
                 rand_pool.free_state(rand_gen);
                 
                 FLOAT sintheta_i = Kokkos::sqrt(1.0-costheta_i*costheta_i);
@@ -197,9 +227,8 @@ int main(int argc, char** argv) {
                 packets.directions(DIM*idx+1) = dir_y;
                 packets.directions(DIM*idx+2) = dir_z;
                 packets.weights(idx) = weight;
+                packets.wavelengths(idx) = invCDF_input_spectrum(r_uniform);
                 packets.isalive(idx) = true;
-                
-                rand_pool.free_state(rand_gen);
             });
             host_packets_num += NPACKETS;
             Kokkos::deep_copy(packets.num, host_packets_num);
@@ -293,7 +322,14 @@ int main(int argc, char** argv) {
                     packets.num() = update;
                 }
             });
-            host_packets_num = packets.num();  // update number of packets host side
+            host_packets_num = packets.num(); // update number of packets host side
+            
+            // check if convergence reached
+            if (abs(FLOAT(old_packets_num) - FLOAT(host_packets_num)) < cnv_tol*old_packets_num) {
+                // end simulation early if reaches a convergence criterion (< 1% change in total energy)
+                // note that right now, this is just tracing the number of packets and assumes all packets have same energy
+                break;
+            }
             
             time += dt;
         }
@@ -318,41 +354,50 @@ int main(int argc, char** argv) {
             }
             
             for (UINT i=0; i < host_packets.num(); ++i) {
-                outputFile << host_packets.locations[3*i] << " " << host_packets.locations[3*i+1] << " " << host_packets.locations[3*i+2] << " " << host_packets.directions[3*i] << " " << host_packets.directions[3*i+1] << " " << host_packets.directions[3*i+2] << " " << host_packets.weights(i) << std::endl;
+                outputFile << host_packets.locations[3*i] << " " << host_packets.locations[3*i+1] << " " << host_packets.locations[3*i+2] << " " << host_packets.directions[3*i] << " " << host_packets.directions[3*i+1] << " " << host_packets.directions[3*i+2] << " " << host_packets.weights(i) << " " << host_packets.wavelengths(i) << std::endl;
             }
             outputFile.close();
         }
         
         // dump energy density z slice
         if (zslice_dump){
-            const FLOAT L = 0.1;
-            const UINT xbins = (DOMAIN_XMAX - DOMAIN_XMIN)/L;
-            const UINT ybins = (DOMAIN_YMAX - DOMAIN_YMIN)/L;
-            const UINT zbins = 1;
-            vector<FLOAT> energy_density(xbins*ybins*zbins);
             
-            #pragma omp parallel for
-            for (UINT i=0; i < host_packets.num(); ++i) {
-                if (host_packets.locations[3*i+2] < -0.5*L || 0.5*L < host_packets.locations[3*i+2]) {
-                    continue;
+            vector<FLOAT> wavs = {0.5, 1.0, 2.0};
+            
+            for (FLOAT wav : wavs) {
+                const FLOAT L = 0.1;
+                const UINT xbins = (DOMAIN_XMAX - DOMAIN_XMIN)/L;
+                const UINT ybins = (DOMAIN_YMAX - DOMAIN_YMIN)/L;
+                const UINT zbins = 1;
+                vector<FLOAT> energy_density(xbins*ybins*zbins);
+                
+                #pragma omp parallel for
+                for (UINT i=0; i < host_packets.num(); ++i) {
+                    if (host_packets.locations[3*i+2] < -0.5*L || 0.5*L < host_packets.locations[3*i+2] || abs(host_packets.wavelengths[i] - wav) > 1e-4) {
+                        continue;
+                    }
+                    
+                    UINT x_idx = (host_packets.locations[3*i] - DOMAIN_XMIN)/L;
+                    UINT y_idx = (host_packets.locations[3*i+1] - DOMAIN_YMIN)/L;
+                    energy_density[x_idx + y_idx*ybins] += host_packets.weights[i]/(L*L*L);
                 }
                 
-                UINT x_idx = (host_packets.locations[3*i] - DOMAIN_XMIN)/L;
-                UINT y_idx = (host_packets.locations[3*i+1] - DOMAIN_YMIN)/L;
-                energy_density[x_idx + y_idx*ybins] += host_packets.weights[i]/(L*L*L);
-            }
-            
-            vector<FLOAT> xs(xbins*ybins*zbins);
-            vector<FLOAT> ys(xbins*ybins*zbins);
-            
-            #pragma omp parallel for
-            for (UINT x_idx=0; x_idx < xbins; ++x_idx) {
-                for (UINT y_idx=0; y_idx < ybins; ++y_idx) {
-                    xs[x_idx + y_idx*ybins] = (x_idx+0.5)*(DOMAIN_XMAX - DOMAIN_XMIN)/xbins + DOMAIN_XMIN;
-                    ys[x_idx + y_idx*ybins] = (y_idx+0.5)*(DOMAIN_YMAX - DOMAIN_YMIN)/ybins + DOMAIN_YMIN;
+                vector<FLOAT> xs(xbins*ybins*zbins);
+                vector<FLOAT> ys(xbins*ybins*zbins);
+                
+                #pragma omp parallel for
+                for (UINT x_idx=0; x_idx < xbins; ++x_idx) {
+                    for (UINT y_idx=0; y_idx < ybins; ++y_idx) {
+                        xs[x_idx + y_idx*ybins] = (x_idx+0.5)*(DOMAIN_XMAX - DOMAIN_XMIN)/xbins + DOMAIN_XMIN;
+                        ys[x_idx + y_idx*ybins] = (y_idx+0.5)*(DOMAIN_YMAX - DOMAIN_YMIN)/ybins + DOMAIN_YMIN;
+                    }
                 }
+                
+                string fn = "energy_density_zslice_";
+                fn.append(std::to_string(int(10*wav)));
+                fn.append(".txt");
+                dump_vector_textfile(fn, xs, ys, energy_density);
             }
-            dump_vector_textfile("energy_density_zslice.txt", xs, ys, energy_density);
         }
         
         Kokkos::printf("Terminating program.\n");
